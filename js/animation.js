d23c0ac64faa930363678ca9de81624ca1431c8c
teli203 <teli203@aol.com>
2024-03-07 14:43:24 -0600

    animation for all 4 corners

--- js/animation.js
+++ js/animation.js
@@ -52,3 +52,140 @@ function handleWindowResize(){
 // Store the last reverse animation, ready to be played //
 let lastReverseAnimation = "";
 
+// Play animation //
+function playAnimation(animation, reverseAnimation) {
+    
+}
+
+function playClosingAnimation(reverseAnimation) {
+
+}
+
+// Onclick Corner Btn functions //
+// top left btn About Author & Blog//
+tlBtn.onclick = function () {
+    if (activeCorner === "top-left") {
+        playClosingAnimation(reverse-animate-top-left);
+    } else {
+        // Set active corner //
+        activeCorner = "top-left";
+        tlBtn.innerHTML = "&uarr;<br />About";
+
+        handleWindowResize();
+        playAnimation("animate-top-left", "reverse-animate-top-left");
+
+        // Change Background Colors //
+        trBtn.style.background = bgColor;
+        brBtn.style.background = bgColor;
+        blBtn.style.background = bgColor;
+        tlBtn.style.background = bgColorAlt;
+
+        // Change Text Colors //
+        trBtn.style.color = textColor;
+        brBtn.style.color = textColor;
+        blBtn.style.color = textColor;
+        tlBtn.style.color = textColorAlt;
+
+        // Change of the Corners Content //
+        trContent.style.transform = trHidden;
+        brContent.style.transform = brHidden;
+        blContent.style.transform = blHidden;
+        tlContent.style.transform = tlActive;
+    }
+};
+
+// top right btn book 3//
+trBtn.onclick = function () {
+    if (activeCorner === "top-right") {
+        playClosingAnimation(reverse-animate-top-right);
+    } else {
+        // Set active corner //
+        activeCorner = "top-right";
+        tlBtn.innerHTML = "&uarr;<br />Triumph";
+
+        handleWindowResize();
+        playAnimation("animate-top-right", "reverse-animate-top-right");
+
+        // Change Background Colors //
+        trBtn.style.background = bgColor;
+        brBtn.style.background = bgColor;
+        blBtn.style.background = bgColor;
+        tlBtn.style.background = bgColorAlt;
+
+        // Change Text Colors //
+        trBtn.style.color = textColor;
+        brBtn.style.color = textColor;
+        blBtn.style.color = textColor;
+        tlBtn.style.color = textColorAlt;
+
+        // Change of the Corners Content //
+        trContent.style.transform = trHidden;
+        brContent.style.transform = brHidden;
+        blContent.style.transform = blHidden;
+        tlContent.style.transform = tlActive;
+    }
+};
+
+// bottom left btn  book 1//
+blBtn.onclick = function () {
+    if (activeCorner === "bottom-left") {
+        playClosingAnimation(reverse-animate-bottom-left);
+    } else {
+        // Set active corner //
+        activeCorner = "bottom-left";
+        tlBtn.innerHTML = "&uarr;<br />Settle";
+
+        handleWindowResize();
+        playAnimation("animate-bottom-left", "reverse-animate-bottom-left");
+
+        // Change Background Colors //
+        trBtn.style.background = bgColor;
+        brBtn.style.background = bgColor;
+        blBtn.style.background = bgColor;
+        tlBtn.style.background = bgColorAlt;
+
+        // Change Text Colors //
+        trBtn.style.color = textColor;
+        brBtn.style.color = textColor;
+        blBtn.style.color = textColor;
+        tlBtn.style.color = textColorAlt;
+
+        // Change of the Corners Content //
+        trContent.style.transform = trHidden;
+        brContent.style.transform = brHidden;
+        blContent.style.transform = blHidden;
+        tlContent.style.transform = tlActive;
+    }
+};
+
+// bottom right btn book 3//
+brBtn.onclick = function () {
+    if (activeCorner === "bottom-right") {
+        playClosingAnimation(reverse-animate-bottom-right);
+    } else {
+        // Set active corner //
+        activeCorner = "bottom-right";
+        tlBtn.innerHTML = "&uarr;<br />Defeat";
+
+        handleWindowResize();
+        playAnimation("animate-bottom-right", "reverse-animate-bottom-right");
+
+        // Change Background Colors //
+        trBtn.style.background = bgColor;
+        brBtn.style.background = bgColor;
+        blBtn.style.background = bgColor;
+        tlBtn.style.background = bgColorAlt;
+
+        // Change Text Colors //
+        trBtn.style.color = textColor;
+        brBtn.style.color = textColor;
+        blBtn.style.color = textColor;
+        tlBtn.style.color = textColorAlt;
+
+        // Change of the Corners Content //
+        trContent.style.transform = trHidden;
+        brContent.style.transform = brHidden;
+        blContent.style.transform = blHidden;
+        tlContent.style.transform = tlActive;
+    }
+};
