50ee6895fc85ec1de3dc25140675a76334d7e448
teli203 <teli203@aol.com>
2024-03-07 11:31:46 -0600

    add const tl, btn, cnt

--- js/animation.js
+++ js/animation.js
@@ -1,2 +1,18 @@
 // DOM Elements //
-const heroImage = document.querySelector("hero")
+const heroImage = document.querySelector("hero__animation__img");
+
+const tl = document.querySelector("#grid__tl");
+const tr = document.querySelector("#grid__tr");
+const bl = document.querySelector("#grid__bl");
+const br = document.querySelector("#grid__br");
+
+const tlBtn = document.querySelector("#grid__tl__btn");
+const trBtn = document.querySelector("#grid__tr__btn");
+const blBtn = document.querySelector("#grid__bl__btn");
+const brBtn = document.querySelector("#grid__br__btn");
+
+const tlContent = document.querySelector("#grid__tl__content");
+const trContent = document.querySelector("#grid__tr__content");
+const blContent = document.querySelector("#grid__bl__content");
+const brContent = document.querySelector("#grid__br__content");
+
