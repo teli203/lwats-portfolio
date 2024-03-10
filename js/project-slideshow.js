6534d6a1f3ee630abb0ed890cd567d78d55af8f0
teli203 <teli203@aol.com>
2024-03-09 18:26:25 -0600

    add comment on slideshow code

--- js/project-slideshow.js
+++ js/project-slideshow.js
@@ -4,17 +4,17 @@ let slideIndex = 1;
 showProject(slideIndex);
 
 function showProject(num) {
-//  //
+// Goes to first pjt after click from last btn //
     if (num > project.length) {
         slideIndex = 1;
 }
 
-//  //
+// Goes to last pjt after clicking backwards from first pjt  //
 if (num < 1) {
     slideIndex = project.length;
 }
 
-//  //
+//hides all pjts  //
 for (let i = 0; i < project.length; i++) {
     project[i].style.display = "none";
 }
@@ -22,6 +22,6 @@ project[slideIndex - 1].style.display = "flex";
 }
 
 function navigateProject(num) {
-    //  //
+    // Changes the slide index based on the back or forward click of the arrow //
     showProject((slideIndex += num));
 }
