5eaa2c0fd436bd7706a96ae18f3b4b6247659bac
teli203 <teli203@aol.com>
2024-03-09 18:20:48 -0600

    finalized slideshow for pjts

--- js/project-slideshow.js
+++ js/project-slideshow.js
@@ -7,5 +7,21 @@ function showProject(num) {
 //  //
     if (num > project.length) {
         slideIndex = 1;
-    }
+}
+
+//  //
+if (num < 1) {
+    slideIndex = project.length;
+}
+
+//  //
+for (let i = 0; i < project.length; i++) {
+    project[i].style.display = "none";
+}
+project[slideIndex - 1].style.display = "flex";
+}
+
+function navigateProject(num) {
+    //  //
+    showProject((slideIndex += num));
 }
